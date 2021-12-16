58b763c1b1073605ee6b6b5e241d4f9ffe14147f
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2021-12-16 10:15:10 +0000

    updated dataTest files

--- js/dataTest.js
+++ js/dataTest.js
@@ -1,17 +1,16 @@
 (function(){
-    console.log('Welcome, if you can see this log app.js works! :)');
+    console.log('Welcome, if you can see this log dataTest.js works! :)');
     console.log('Feel free to play around with it.');
 })();
 
 function getData(){
-    console.log('get data');
-
     fetch('https://justfields.com/project/eNyAAPy7/json')
     .then( response => response.json() )
     .then(function (data) {
-        console.log(data.confirmationgroup.confirmation);
+        // console.log(data.confirmationgroup.confirmation);
+        console.log('Data incoming from Justfields.com');
         data.confirmationgroup.confirmation.forEach(function(confirmation){
             console.log(confirmation.name +', '+ confirmation.email +', '+ confirmation.phone +', '+ confirmation.date +', '+ confirmation.time +', '+ confirmation.gender +', '+ confirmation.style );
-        })
-    })
-}
+        });
+    });
+};

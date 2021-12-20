b03476df99dbc3ee26ac8ce9d67046db64ee4740
Brigitta-Nagy <brigitta.nagy@bgreat.co.uk>
2021-12-20 12:27:40 +0000

    gallery-1st-edition-done

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,3 +1,14 @@
-(function(){
-    alert('alert');
-}());
+const images = document.querySelectorAll('.image-gallery'); 
+
+images.forEach(function(image){
+    image.addEventListener('mouseover', ()=>{
+        removeLargeCLassName()
+        image.classList.add('large')
+    })
+})
+function removeLargeCLassName(){
+    images.forEach(image =>{
+        image.classList.remove('large')
+    })
+    
+}

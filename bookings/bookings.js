ea41eaf7f05b14256206ca2e503b208d2d69a636
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2021-12-26 07:55:33 +0000

    added form validation

--- bookings/bookings.js
+++ bookings/bookings.js
@@ -13,41 +13,52 @@ let spacer = document.getElementById('spacer');
 // functions needed for dropdown buttons
 
 document.getElementById("gents").addEventListener('click',function(e){
-    if(hairStyleChoice.innerText !== ""){
-        hairStyleChoice.innerText = "";    
+    if(hairStyleChoice.value !== ""){
+        hairStyleChoice.value = "";    
     }
     gender = e.target.innerText;   
     // console.log(gender);
-    choice.innerText = gender;
+    choice.value = gender;
     gentsCuts.classList.remove('visually-hidden');
     ladiesCuts.classList.add('visually-hidden');
     kidsCuts.classList.add('visually-hidden');
+    document.getElementById("genderFeedback").classList.add('visually-hidden');
+    document.getElementById("hairStyleFeedback").classList.add('visually-hidden');
+    document.getElementById("errorMessages").classList.add('visually-hidden');
     spacer.remove();
 });    
 
 document.getElementById("ladies").addEventListener('click',function(e){
-    if(hairStyleChoice.innerText !== ""){
-        hairStyleChoice.innerText = "";    
+    if(hairStyleChoice.value !== ""){
+        hairStyleChoice.value = "";    
     }
     gender = e.target.innerText;   
     // console.log(gender);
-    choice.innerText = gender;
+    choice.value = gender;
     gentsCuts.classList.add('visually-hidden');
     ladiesCuts.classList.remove('visually-hidden');
     kidsCuts.classList.add('visually-hidden');
+    document.getElementById("genderFeedback").classList.add('visually-hidden');
+    document.getElementById("hairStyleFeedback").classList.add('visually-hidden');
+    document.getElementById("errorMessages").classList.add('visually-hidden');
+
     spacer.remove();   
 });    
 
 document.getElementById("kids").addEventListener('click',function(e){
-    if(hairStyleChoice.innerText !== ""){
-        hairStyleChoice.innerText = "";    
+    if(hairStyleChoice.value !== ""){
+        hairStyleChoice.value = "";    
     }
     gender = e.target.innerText;   
     // console.log(gender);
-    choice.innerText = gender;
+    choice.value = gender;
     gentsCuts.classList.add('visually-hidden');
     ladiesCuts.classList.add('visually-hidden');
     kidsCuts.classList.remove('visually-hidden');
+    document.getElementById("genderFeedback").classList.add('visually-hidden');
+    document.getElementById("hairStyleFeedback").classList.add('visually-hidden');
+    document.getElementById("errorMessages").classList.add('visually-hidden');
+
     spacer.remove();   
 });  
 
@@ -56,7 +67,8 @@ elements = document.getElementsByClassName('hairstyle');
 Array.from(elements).forEach((element) => {
     element.addEventListener('click', (event) => {
         // console.log(event.target.innerText);
-        document.querySelector('#hairStyleChoice').innerText = event.target.innerText;
+        document.querySelector('#hairStyleChoice').value = event.target.innerText;
+        document.getElementById("type").style.border = "1px solid green";
     });
 });
 
@@ -66,11 +78,51 @@ Array.from(elements).forEach((element) => {
 
 
 // functions needed for submit
+
+// form validator function
+(function(){
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+    let yyyy = today.getFullYear();
+    if(dd<10){
+      dd='0'+dd
+    } 
+    if(mm<10){
+      mm='0'+mm
+    } 
+    
+    today = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("date").setAttribute("min", today);
+    // console.log('datesetter IIFE working.');
+})();
+
+(function() {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
+
+
 function submitForm(){
     let data = collectData();
-    console.log("This is the JSON object that we can send to the backend: " + JSON.stringify(data));
+    alert("This is the JSON object that we can send to the backend: " + JSON.stringify(data));
     // this is where we can send the HTTP request with the JSON data from
+    checkDropdownValues();
     resetForm();
+    // event.preventDefault();
 } 
 
 function collectData(){
@@ -79,8 +131,8 @@ function collectData(){
     let phone = document.getElementById('phone').value;    
     let date = document.getElementById('date').value;    
     let time = document.getElementById('time').value;
-    let gender = document.getElementById('choice').innerText;
-    let style = document.getElementById('hairStyleChoice').innerText; 
+    let gender = document.getElementById('choice').value;
+    let style = document.getElementById('hairStyleChoice').value; 
     
     let Booking = {
         name : name,
@@ -94,8 +146,23 @@ function collectData(){
     return Booking;
 }
 
+function checkDropdownValues(){
+    console.log('check dropdown');
+    if(choice.value === ""){
+        document.getElementById("genderFeedback").classList.remove('visually-hidden');
+        document.getElementById("errorMessages").classList.remove('visually-hidden');
+        document.getElementById("type").style.border = "1px solid #dc3545";
+    }
+
+    if(hairStyleChoice.value === ""){
+        document.getElementById("hairStyleFeedback").classList.remove('visually-hidden');
+        document.getElementById("errorMessages").classList.remove('visually-hidden');
+        document.getElementById("type").style.border = "1px solid #dc3545";
+    }
+}
+
 function resetForm(){
-    document.getElementById('inputForm').reset();
+    // document.getElementById('inputForm').reset();
     choice.innerText = "";
     hairStyleChoice.innerText = "";
     if(gentsCuts){
